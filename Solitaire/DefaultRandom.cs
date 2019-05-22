3d3ce0aaaaad26c741c8f96644232721268142aa
lionel-git <lionel.desorme@free.fr>
2019-05-22 10:01:10 +0200

    Test simple random LCG generator

---
+++ Solitaire/DefaultRandom.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Solitaire
+{
+    public class DefaultRandom : IRandom
+    {
+        private Random _random;
+        private BinaryFormatter _formatter = new BinaryFormatter();
+        private MemoryStream _saveStream = new MemoryStream();
+
+        public DefaultRandom(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public void BackupState()
+        {
+            _saveStream.Seek(0, SeekOrigin.Begin);
+            _formatter.Serialize(_saveStream, _random);
+        }
+
+        public int Next(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        public void RestoreState()
+        {
+            _saveStream.Seek(0, SeekOrigin.Begin);
+            _random = (Random)_formatter.Deserialize(_saveStream);
+        }
+    }
+}
---
+++ Solitaire/RandomLCG.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Solitaire
+{
+    public class RandomLCG : IRandom
+    {
+        private readonly uint _a;
+        private readonly uint _c;
+
+        private uint _state = 0;
+        private uint _backupState = 0;
+
+        public RandomLCG(uint seed, uint a = 1664525, uint c = 1013904223)
+        {
+            _a = a;
+            _c = c;
+            _state = seed;
+        }
+
+        public void BackupState()
+        {
+            _backupState = _state;
+        }
+
+        public int Next(int min, int max)
+        {
+            _state = _a * _state + _c;
+            return (int)(((UInt64)(max - min) * (UInt64)(_state)) >> 32);
+        }
+
+        public void RestoreState()
+        {
+            _state = _backupState;
+        }
+    }
+}
